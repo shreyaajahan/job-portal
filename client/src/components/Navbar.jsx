074e73528371a44b37f6c08aad8798df759f1d53
shreyaa <shreyaajahan@gmail.com>
2025-08-07 00:02:01 +0530

    ApplyJob page is enhanced more

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { assets } from '../assets/assets';
 import { useClerk, UserButton, useUser, SignedIn, SignedOut } from '@clerk/clerk-react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
   const { openSignIn, openUserProfile } = useClerk();
   const { user } = useUser();
 
+  const navigate=useNavigate();
+
   return (
     <div className='shadow py-4'>
       <div className='container px-4 2xl:px-20 mx-auto flex justify-between items-center'>
-        <img src={assets.logo} alt="Logo" className='h-10' />
+        <img
+  onClick={() => navigate('/')}
+  src={assets.logo}
+  alt="Logo"
+  className="cursor-pointer h-10"
+/>
 
         <div className='flex gap-4 max-sm:text-xs items-center'>
           {/* Optional: Recruiter login button */}
