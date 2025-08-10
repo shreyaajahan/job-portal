5532696f339b468b325364247bc2dec8d3732d3a
shreyaa <shreyaajahan@gmail.com>
2025-08-10 18:47:05 +0530

    Completed Authentication of Company

--- server/server.js
+++ server/server.js
@@ -7,12 +7,14 @@ import connectDB from './config/db.js';
 import * as Sentry from "@sentry/node";
 import { clerkWebHooks } from './controllers/webhooks.js';
 import companyRoutes from './routes/companyRoutes.js'
+import connectCloudinary from './config/cloudinary.js';
 
 // Initialize Express
 const app = express();
 
 // Connect to database
 await connectDB();
+await connectCloudinary();
 
 // Middlewares
 app.use(cors());
