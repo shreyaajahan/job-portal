96523d681dce088353793f3afe8e6662fa7a4bd8
shreyaa <shreyaajahan@gmail.com>
2026-02-02 23:14:42 +0530

    Fix CORS for production deployment

--- server/server.js
+++ server/server.js
@@ -21,8 +21,10 @@ await connectCloudinary();
 
 // Middlewares
 app.use(cors({
-  origin: '*',
-  credentials: true
+  origin: ['http://localhost:5173', 'https://job-portal-client-three-psi.vercel.app'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
